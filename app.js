bd897df6165d51c7febcf9e4130d7a055bd63c50
Aaron Cochran <acochran89@gmail.com>
2014-06-25 03:02:49 -0500

    Add app files
    added to index.html. Added style.css, app.js, and a background image for
    the Angular ToDo app.

---
+++ app.js
@@ -0,0 +1,38 @@
+//Define angular app
+var app = angular.module('TaskManager', []); 
+
+//controllers
+app.controller('taskController', function($scope) {
+    $scope.today = new Date();
+    $scope.saved = localStorage.getItem('taskItems');
+    $scope.taskItem = (localStorage.getItem('taskItems')!==null) ? 
+    JSON.parse($scope.saved) : [ {description: "Why not add a task?", date: $scope.today, complete: false}];
+    localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    
+    $scope.newTask = null;
+    $scope.newTaskDate = null;
+    $scope.addNew = function () {
+        $scope.taskItem.push({
+            description: $scope.newTask,
+            date: $scope.newTaskDate,
+            complete: false
+        });
+        $scope.newTask = '';
+        $scope.newTaskDate = '';
+        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    };
+    $scope.deleteTask = function () {
+        var completedTask = $scope.taskItem;
+        $scope.taskItem = [];
+        angular.forEach(completedTask, function (taskItem) {
+            if (!taskItem.complete) {
+                $scope.taskItem.push(taskItem);
+            }
+        });
+        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    };
+    
+    $scope.save = function () {
+        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    }
+});
